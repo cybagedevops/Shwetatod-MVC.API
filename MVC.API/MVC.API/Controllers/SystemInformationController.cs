6b57ec605507a1f41eb023e14f0d741d47a342d3
Shweta <shwetatod@cybage.com>
2021-10-28 18:34:27 +0530

    MVC-API

---
+++ MVC.API/MVC.API/Controllers/SystemInformationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Web.Http;
+using System.Web.Http;
+using WebAPIHandson.Models;
+
+namespace WebAPIHandson.Controllers
+{
+    public class SystemInformationController : ApiController
+    {
+        public List<SystemInformation> GetSystemInformation()
+        {
+            List<SystemInformation> info = new List<SystemInformation>();
+            String sysIp = "";
+
+            String sysOs = "";
+
+
+            String machineName1 = Environment.MachineName;
+
+            SystemInformation SysInfo = new SystemInformation();
+
+            SysInfo.MachineName = machineName1;
+
+            System.OperatingSystem osInfo = System.Environment.OSVersion;
+
+            sysOs = osInfo.Platform.ToString();
+
+            SysInfo.OsName = sysOs;
+
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    sysIp = ip.ToString();
+                }
+            }
+
+            SysInfo.IPAddress = sysIp;
+
+            info.Add(SysInfo);
+            return info;
+        }
+
+    }
+}
